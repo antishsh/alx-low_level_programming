573fe1830d8971537ffae68fb78d32b9be76d783
antishsh <antenehshimelis2019@gmail.com>
2022-04-07 10:41:43 -0700

    last

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - The purpose of this file is to output
- *
- *   * the string: with proper grammar, but the outcome is a piece of art,
- *
- *    * followed by a newline, using printf and not puts.
- *
- *     * Return: Always 0 (Success)
- *
- *      */
+ * main - The purpose of this file is to output
+ * the string: with proper grammar, but the outcome is a piece of art,
+ * followed by a newline, using printf and not puts.
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - This program prints the size of various types
- *
- *   * on the computer it is compiled and run on.
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - This program prints the size of various types
+ * on the computer it is compiled and run on.
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
