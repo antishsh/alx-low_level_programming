d4eabe54ebf9cc94d9305ee9af6f458c1880aa8f
antishsh <antenehshimelis2019@gmail.com>
2022-04-08 12:37:12 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,3 @@
-
-   
 #include <stdio.h>
 
 /**
@@ -10,12 +8,9 @@ int main(void)
 {
 		int i;
 
-			for (i = 48; i < 58; i++)
-				
+			for (i = 48; i < 58; i++)	
 			{
-		
 				putchar(i);
-
 			}
 				putchar('\n');
 					return (0);
