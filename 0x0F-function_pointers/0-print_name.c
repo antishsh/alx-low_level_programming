32f9694d3e07b5042d30147084a0be21ada8813f
Anteneh Shimelis <48884352+antishsh@users.noreply.github.com>
2022-05-11 21:58:48 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+include "function_pointers.h"
+
 /**
- *print_name - calls another function to peint a name
- *
- *@name:name to print
- *@f:function name
- *
- *Return:void
- */
+*print_name - prints a name
+*@name:name to print
+*@f: function which print name
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
-	{
-		return;
-	}
-	(*f)(name);
+
+if (name == NULL || f == NULL)
+return;
+
+f(name);
+
 }
