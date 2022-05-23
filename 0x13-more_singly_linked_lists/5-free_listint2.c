c6038f59ccef3b91fb157e985b1a1b1d86134abf
Anteneh Shimelis <48884352+antishsh@users.noreply.github.com>
2022-05-23 20:03:43 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,9 @@
 #include "lists.h"
 
 /**
-*free_listint2 - frees a linked list
-*@head: pointer to the head of the list
-*
-*Return: void
-*/
+ * free_listint2 - Frees a list.
+ * @head: Address of the first node of a list.
+ **/
 
 void free_listint2(listint_t **head)
 {
