72c4044f3c8416eca3ffff72e124081a3103c71d
antishsh <antenehshimelis2019@gmail.com>
2022-07-28 12:50:17 -0700

    correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,24 +7,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+
+	hash_table_t *hash_table;
 
 	if (size == 0)
 		return (NULL);
 
-	ht = malloc(sizeof(hash_table_t));
+	hash_table = malloc(sizeof(hash_table_t));
 
-	if (ht == NULL)
+	if (hash_table == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = calloc(size, sizeof(hash_node_t *) * size);
-
-	if (ht->array == NULL)
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(ht);
+		free(hash_table);
 		return (NULL);
 	}
-	
-	return (ht);
+	return (hash_table);
 }
